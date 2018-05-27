658e72887bf2df9f7aa7ca2fe0f4369b21786de8
mbefore305 <michael.bey@posteo.de>
2018-05-27 13:07:07 +0200

    t3

--- app/src/main/java/com/example/michael/gittest3/MainActivity.java
+++ app/src/main/java/com/example/michael/gittest3/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.michael.gittest3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+// Test 3 Home
 public class MainActivity extends AppCompatActivity {
 
     @Override
