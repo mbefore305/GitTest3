f485da221676410c428b8be7df2326235f3ede92
mbefore305 <michael.bey@posteo.de>
2018-05-28 20:28:23 +0200

    home 28.5. 2

--- app/src/main/java/com/example/michael/gittest3/MainActivity.java
+++ app/src/main/java/com/example/michael/gittest3/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 // Test 3 Home
 // Updated 2018-05-28 10:25 at work
+// Again updated at home 28.05
 public class MainActivity extends AppCompatActivity {
 
     @Override
